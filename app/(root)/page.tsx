c5b6a222be7cadb0aaf642b0d06505d02d977812
Raj Vardhan Singh Birani <rvsb@Rajs-MacBook-Pro.local>
2024-07-10 14:46:48 +0530

    FEAT: initialized shadcn, made route groups for auth and root, initialized clerk auth, implemented sidebar and mobile navbar

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+type Props = {};
+
+const Home = (props: Props) => {
+  return <div></div>;
+};
+
+export default Home;
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const TransformationsPage = (props: Props) => {
+  return <div>TransformationsPage</div>;
+};
+
+export default TransformationsPage;
